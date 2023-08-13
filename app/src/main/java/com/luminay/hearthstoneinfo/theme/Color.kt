c74ee8f9dd7b32213c23b6910f99a5fd426ae7af
Lucas Coelho <lcsilva2099@gmail.com>
2023-08-13 14:57:24 -0300

    Removing unused resources

--- app/src/main/java/com/luminay/hearthstoneinfo/theme/Color.kt
+++ app/src/main/java/com/luminay/hearthstoneinfo/theme/Color.kt
@@ -9,7 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Yellow20 = Color(0xFFFFE5AC)
 val Yellow30 = Color(0xFFFFFF94)
 val Yellow90 = Color(0xFFFFD700)
-val Yellow100 = Color(0xFFFFEB3B)
 
 val Red90 = Color(0xFF390C0C)
 val Red100 = Color(0xFF200A0A)
@@ -19,9 +18,5 @@ val Orange80 = Color(0xFFAC6726)
 val Orange100 = Color(0xFF865220)
 
 val Brown20 = Color(0xFFA38970)
-val Brown80 = Color(0xFF614326)
 
-val Gray40 = Color(0xFF444444)
 val Gray80 = Color(0xFF6F6E6C)
-
-val Black50 = Color(0xFF141414)
